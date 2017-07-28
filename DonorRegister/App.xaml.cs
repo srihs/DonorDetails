cb4c48572baabd8715945e09b157fdebaa0b6480
ssamarasooriya <srimalhs@gmail.com>
2017-07-28 07:35:53 +0530

    initial commit

---
+++ DonorRegister/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DonorRegister
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private const int MINIMUM_SPLASH_TIME = 1500; // Miliseconds
+        private const int SPLASH_FADE_TIME = 500;     // Miliseconds
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Step 1 - Load the splash screen
+            SplashScreen splash = new SplashScreen("images/Logo2.png");
+            splash.Show(false, true);
+
+            // Step 2 - Start a stop watch
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            // Step 3 - Load your windows but don't show it yet
+            base.OnStartup(e);
+            Login frmLogin = new Login();
+
+            // Step 4 - Make sure that the splash screen lasts at least two seconds
+            timer.Stop();
+            int remainingTimeToShowSplash = MINIMUM_SPLASH_TIME - (int)timer.ElapsedMilliseconds;
+            if (remainingTimeToShowSplash > 0)
+                Thread.Sleep(remainingTimeToShowSplash);
+
+            // Step 5 - show the page
+            splash.Close(TimeSpan.FromMilliseconds(SPLASH_FADE_TIME));
+            frmLogin.Show();
+        }
+    }
+}
---
+++ DonorRegister/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DonorRegister
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddNewUser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AddDonor objAddDonor = new AddDonor();
+                objAddDonor.Show();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+    }
+}
