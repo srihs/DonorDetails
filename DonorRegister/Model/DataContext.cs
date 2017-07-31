1cf5ec2f0ca24d10725b6dfa0e72c3ac3c6cc875
Srimal <srimalhs@gmail.com>
2017-07-31 21:25:11 +0530

    Add DbContext

---
+++ DonorRegister/Model/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonorRegister.Model
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DonorRegistery")
+        { }
+
+        public DbSet<Donor> Donor { get; set; }
+    }
+}
