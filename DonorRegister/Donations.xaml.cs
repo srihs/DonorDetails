60ba5f0c8a3b454c700645ea1e5c1b8688411c90
Srimal <srimalhs@gmail.com>
2017-08-12 13:57:06 +0530

    Completed Donation form saving operation

--- DonorRegister/Donations.xaml.cs
+++ DonorRegister/Donations.xaml.cs
@@ -34,10 +34,11 @@ namespace DonorRegister
         {
             InitializeComponent();
             dbContext = new DonorDbContext();
+            dtpDonationDate.SelectedDate = System.DateTime.Now.Date;
 
             try
             {
-                List<Donor> objDonorList = dbContext.Donors.ToList<Donor>();
+                //List<Donor> objDonorList = dbContext.Donors.ToList<Donor>();
                 objDonorListDonation = new List<Donation>();
             }
             catch (Exception ex)
@@ -51,12 +52,25 @@ namespace DonorRegister
         {
             try
             {
+                if (objDonorListDonation != null)
+                {
+                    foreach (Donation donation in objDonorListDonation)
+                    {
+                        dbContext = new DonorDbContext();
+                        dbContext.Donations.Add(donation);
+                        dbContext.SaveChanges();
+                    }
+
+                    MessageBox.Show("Record saved", "Saved");
+                    objDonorListDonation = null;
+                    dgDonations.ItemsSource = null;
+                }
 
             }
-            catch (Exception)
+            catch (Exception ex)
             {
 
-                throw;
+                MessageBox.Show(ex.Message);
             }
         }
 
@@ -64,6 +78,16 @@ namespace DonorRegister
         {
             try
             {
+
+                if (objDonorListDonation == null)
+                    objDonorListDonation = new List<Donation>();
+
+                if (txtMemberNo.Text == string.Empty || txtAmount.Text == string.Empty)
+                {
+                    txtAmount.BorderBrush = Brushes.Red;
+                    txtMemberNo.BorderBrush = Brushes.Red;
+                    return;
+                }
                 objDonor = new Donor();
                 objDonor = dbContext.Donors.SingleOrDefault(donor => donor.MembershipNo == txtMemberNo.Text);
                 if (objDonor != null)
@@ -72,12 +96,13 @@ namespace DonorRegister
                     Donation objDonation = new Donation();
                     objDonation.DonorId = objDonor.Id;
                     objDonation.Amount = Double.Parse(txtAmount.Text);
-                    objDonation.Month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(dtpDonationDate.SelectedDate.Value.Month); 
+                    objDonation.Month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(dtpDonationDate.SelectedDate.Value.Month);
                     objDonation.Year = dtpDonationDate.SelectedDate.Value.Year;
                     objDonation.MembershipNo = objDonor.MembershipNo;
                     objDonation.DateCreated = System.DateTime.Now;
                     objDonorListDonation.Add(objDonation);
-
+                    txtMemberNo.Clear();
+                    txtAmount.Clear();
                 }
 
                 dgDonations.ItemsSource = objDonorListDonation;
@@ -87,10 +112,24 @@ namespace DonorRegister
             catch (Exception ex)
             {
 
-                throw;
+                MessageBox.Show(ex.Message);
             }
         }
 
-        
+        private void btnDgDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                objDonorListDonation.RemoveAt(dgDonations.SelectedIndex);
+                dgDonations.ItemsSource = objDonorListDonation;
+                dgDonations.Items.Refresh();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
